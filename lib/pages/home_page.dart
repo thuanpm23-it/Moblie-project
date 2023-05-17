fc4becda02b3b7a86430420b8335ac96868a4c08
Phạm Minh Thuận <thuanpm21.cdsg@saigontech.edu.vn>
2023-05-17 23:09:33 +0700

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/categories_widget.dart';
+import '../widgets/home_app_bar.dart';
+import '../widgets/items_widget.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          const HomeAppBar(),
+          Container(
+            // height: 500,
+            padding: EdgeInsets.only(top: 15),
+            decoration: BoxDecoration(
+              color: Color(0xFFEDECF2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(35),
+                topRight: Radius.circular(35),
+              ),
+            ),
+            child: Column(children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 15),
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 5),
+                      height: 50,
+                      width: 300,
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Search here...",
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    Icon(
+                      Icons.camera_alt,
+                      size: 27,
+                      color: Color(0xFF4C53A5),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.symmetric(
+                  vertical: 20,
+                  horizontal: 10,
+                ),
+                child: Text(
+                  "Categories",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF4C53A5),
+                  ),
+                ),
+              ),
+              CategoriesWidget(),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Text(
+                  "Best Selling",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF4C53A5),
+                  ),
+                ),
+              ),
+              ItemsWidget(),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
