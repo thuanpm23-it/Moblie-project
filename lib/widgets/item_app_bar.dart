202dc6afefceceb1d053aac371c416742c096b1e
Hai <haivanle55627@gmail.com>
2023-05-18 16:22:45 +0700

    update Item Pages

--- lib/widgets/item_app_bar.dart
+++ lib/widgets/item_app_bar.dart
@@ -10,6 +10,40 @@ class ItemAppBar extends StatefulWidget {
 class _ItemAppBarState extends State<ItemAppBar> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.all(25),
+      child: Row(
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(
+              Icons.arrow_back,
+              size: 30,
+              color: Color(0xFF4c53A5),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              "Product",
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF4c53A5),
+              ),
+            ),
+          ),
+          const Spacer(),
+          const Icon(
+            Icons.favorite,
+            size: 30,
+            color: Colors.red,
+          )
+        ],
+      ),
+    );
   }
 }
