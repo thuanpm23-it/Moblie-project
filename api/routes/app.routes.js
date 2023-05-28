9e9101b90b1c96d6acfa4c898a23b192f1861d6a
NguyenThanh_Nhan <nhannt200823@gmail.com>
2023-05-28 21:16:23 +0700

    update code

--- api/routes/app.routes.js
+++ api/routes/app.routes.js
@@ -1,4 +1,6 @@
 const categoryController = require("../controllers/categories.controller");
+const productController = require("../controllers/products.controller");
+
 const express = require("express");
 const router = express.Router();
 
@@ -7,5 +9,12 @@ router.get("/category", categoryController.findAll);
 router.get("/category/:id", categoryController.findOne);
 router.put("/category/:id", categoryController.update);
 router.delete("/category/:id", categoryController.delete);
+
+router.post("/product", productController.create);
+router.get("/product", productController.findAll);
+router.get("/product/:id", productController.findOne);
+router.put("/product/:id", productController.update);
+router.delete("/product/:id", productController.delete);
+
 
 module.exports = router;
