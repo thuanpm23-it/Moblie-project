acb938072579eb5789fbb6103be38614861484b9
NguyenThanh_Nhan <nhannt200823@gmail.com>
2023-05-28 21:18:26 +0700

    update

---
+++ api/models/product.model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const product = mongoose.model(
+    "Product",
+    mongoose.Schema(
+        {
+            productName:{
+                type:String,
+                require: true,
+                unquie: true,
+            },
+            category:{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Category",
+            },
+            productShortDescription:{
+                type: String,
+                required:true
+            },
+            productDescription:{
+                type:String,
+                required:false
+            },
+            productPrice:{
+                type:Number,
+                required:true,
+            },
+            productSalePrice:{
+                type:Number,
+                required:true,
+                default:0
+            },
+            productImage:{
+                type:String,
+
+            },
+            productSKU:{
+                type:String,
+                required:false
+            },
+            productType:{
+                type:String,
+                required:"simple"
+            },
+            stockStatus:{
+                type:String,
+                default:"IN"
+            }
+
+        },{
+        toJSON:{
+            tranform:function(doc,ret){
+                ret.productId = ret._id.toString();
+                delete ret._id;
+                delete ret.__v;
+            }
+        }
+    })
+);
+module.exports = {
+    product 
+}
---
+++ api/services/products.service.js
@@ -0,0 +1,110 @@
+const { product } = require("../models/product.model");
+const { category } = require("../models/category.nodel");
+
+const { MONGO_DB_CONFIG } = require("../config/app.config");
+const { response } = require("express");
+
+async function createProduct(params, callback) {
+  if (!params.productName) {
+    return callback(
+      {
+        message: "Product Name required",
+      },
+      ""
+    );
+  }
+  if (!params.category) {
+    return callback(
+      {
+        message: "Category required",
+      },
+      ""
+    );
+  }
+  const productModel = new product(params);
+  productModel
+    .save()
+    .then((response) => {
+      return callback(null, response);
+    })
+    .catch((error) => {
+      return callback(error);
+    });
+}
+async function getProduct(params, callback){
+    const productName = params.productName;
+    const categoryId = params.categoryId;
+    var condition = {};
+    if(productName){
+        condition["productName"] = {
+            $regex: new RegExp(productName),$options:"i"
+        };
+    }
+    if(categoryId){
+        condition["categoryId"] = categoryId;
+    }
+    let perPage = Math.abs(params.pageSize) || MONGO_DB_CONFIG.PAGE_SIZE;
+    let page = (Math.abs(params.page) || 1) - 1
+    product
+    .find(condition,"productName productShortDescription productDescription productPrice productSalePrice productImage productSKU productType stockStatus ")
+    .populate("category", "categoryName", "categoryImage" )
+    .limit(perPage)
+    .skip(perPage * page)
+    .then((response) => {
+        return callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+async function getProductById(params, callback){
+    const productId = params.productId;
+ 
+    product
+    .findById(productId)
+    .populate("category", "categoryName", "categoryImage" )
+    .then((response) => {
+        return callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+async function updateProduct(params, callback){
+    const productId = params.productId;
+ 
+    product
+    .findByIdAndUpdate(productId,params, {useFindAndModify: false})
+    .then((response) => {
+        if(!response){
+            callback(`Cannot update Product with id ${productId}`)
+        }
+        else callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+async function deleteProduct(params, callback){
+    const productId = params.productId;
+ 
+    product
+    .findByIdAndRemove(productId)
+    .then((response) => {
+        if(!response){
+            callback(`Cannot update Product with id ${productId}`)
+        }
+        else callback(null, response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+module.exports ={
+    createProduct,
+    getProduct,
+    getProductById,
+    updateProduct,
+    deleteProduct
+};
